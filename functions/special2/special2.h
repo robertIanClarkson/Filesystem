8c2709f43e263adbeb17e2e9eea6dabe7486e5d2
Cameron Harte <cameron.j.harte@gmail.com>
2020-04-20 00:06:37 -0700

    Skeleton layout added for the "2 special commands"

---
+++ functions/special2/special2.h
@@ -0,0 +1,10 @@
+#ifndef special2_h
+#define special2_h
+
+#include <stdio.h>
+#include "../../structs.h"
+#include "../../low/fsLow.h"
+
+int special2(struct filesystem_volume volume, struct arguments command);
+
+#endif /* special2_h */
