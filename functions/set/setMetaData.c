4174b9e96806a271a79c835dced1036c47c0ca61
Cameron Harte <cameron.j.harte@gmail.com>
2020-04-23 15:50:53 -0700

    Update setMetaData.c

--- functions/set/setMetaData.c
+++ functions/set/setMetaData.c
@@ -1,5 +1,28 @@
 #include "setMetaData.h"
 
+/* Filesystem_Prompt$ set 'hello' 'world' 'author' 'foo bar' */
+
 int setMetaData(struct filesystem_volume volume, struct arguments command) {
-    return 0;
+    /* Checking argc */
+    if(command.argc != 5) {
+        printf("***Not Enough Args***\n");
+        return 0; // check
+    }
+
+    /* Get args */
+    char* file = command.args[1];
+    char* directory = command.args[2];
+    char* category = command.args[3];
+    char* info = command.args[4];
+
+    /* Get index of 'directory' */
+    printf("- Looking for directory\n");
+    int directoryIndex = getIndex(directory, volume);
+    if(directoryIndex < 0) {
+        printf("***FOLDER DNE***\n");
+        return 0;
+    }
+    printf("- directory index: %d\n", directoryIndex);
+
+    return 1;
 }
