de4a010577b7f2ae678a92b9dba2abc7f5be2e65
Cameron Harte <cameron.j.harte@gmail.com>
2020-04-23 23:51:17 -0700

    Update special1.c

--- functions/special1/special1.c
+++ functions/special1/special1.c
@@ -1,12 +1,5 @@
 #include "special1.h"
 
 int special1(struct filesystem_volume volume, struct arguments command) {
-    /* Basic steps laid out by Bierman - reference simple shell assignment for guidance
-    srcfd = yourOpen(sourcefile)
-    destfd = open(linuxDestinationFile)
-    yourRead(srcfd, buf, len)
-    write(destfd, buf, len)
-    repeat read/write until entire file copied
-    yourClose(srcfd)
-    close(destfd) */
+    
 }
