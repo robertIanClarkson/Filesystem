a72fe37950149fd84b44a6c0c568d4d0699bb555
MichaelZheng911 <michael.zheng.910@gmail.com>
2020-04-24 20:11:31 -0700

    Added Reading

--- functions/special1/special1.c
+++ functions/special1/special1.c
@@ -7,25 +7,53 @@ int special1(struct filesystem_volume volume, struct arguments command) {
     char buffer[BUF_SIZE];
 
     /* check if command.argc != 2 */
-    if(command.argc < 2) {
+    if(command.argc < 4) {
         printf("***Not Enough Args***\n");
         return 0; // check
     }
-    else if(command.argc > 2) {
+    else if(command.argc > 4) {
 	    printf("***TOO many Args***\n");
 	    return 0; //check
     }
 
     // Get args 
     char* sourceFile = command.args[1];
-    char* destinationFile = command.args[2];
+    char* fileName = command.args[2];
+    char* destinationFile = command.args[3];
 
     // Open Linux File 
     int linuxFile = open(linuxFile, O_RDONLY);
+    // Open Destination File
+    int DSFile = open(destinationFile, O_WRONLY | O_CREAT, 0644);
     //negative value is returned, then there was an error opening the file
-    if(linuxFile < 0)
+    if(linuxFile < 0){
+	printf("***ERROR CANNOT OPEN FILE***");
         return 0;
-    
+    }
+    if(linuxFile > 0){
+    	printf("*OPENING THE FILE*");
+	FILE* newFile;
+	newFile = fopen(comman.args[1], "r");
+	if(newFile == NULL)
+    	{
+        /* File not created hence exit */
+        printf("***UNABLE TO CREATE FILE.***\n");
+        return 0;
+    	}
+	else if(newFile != NULL){
+	printf("***CREATING FILE***\n")	
+		while((returnSource = read (linuxFile, &buffer, BUF_SIZE)) > 0){
+            		returnDestination = write (DSFile, &buffer, (ssize_t) returnSource);
+            		if(returnDestination != returnSource){
+                	/* Write error */
+                	perror("***FILE DO NOT MATCH***");
+                	return 4;
+            		}
+   		}
+	}
+	
+	return 1;
+    }
     returnSource = read(linuxFile,&buffer,BUF_SIZE));
 
     if(returnSource < 1)
