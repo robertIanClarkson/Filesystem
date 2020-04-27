975bb989493ac3e0c16e153aefa14fc5574e4433
Cameron Harte <cameron.j.harte@gmail.com>
2020-04-27 15:21:21 -0700

    Removed hardcode for testing, works on a file that is only 1 LBA block

--- functions/special2/special2.c
+++ functions/special2/special2.c
@@ -18,74 +18,68 @@ int special2(struct filesystem_volume volume, struct arguments command) {
         printf("***ERROR FILE DOES NOT EXIST***");
         return 0;
     }
-    printf("1 index\n");
 
     // check last character of path == '/'
     if(linuxDestinationFile[strlen(linuxDestinationFile)-1] != '/') {
         printf("***ERROR PATH IS NOT VALID***");
         return 0;
-    }  
-    printf("2 path\n");
-    
-    printf("%ld\n",strlen(sourceFile));
-    strncat(linuxDestinationFile, sourceFile, 4);
+    } 
+
+    /* seems a little messy, might be a better way to trim '-' from fileName */
+    int len = strlen(sourceFile);   	
+    for(int i = 0; i < len; i++) {
+        if(sourceFile[i] == '-') {
+	    for(int j = i; j < len; j++) {
+		sourceFile[j] = sourceFile[j + 1];
+	    }
+	    len--;
+	    i--;	
+        } 
+    }
+    printf("sourceFile: %s\n", sourceFile);
+    strcat(linuxDestinationFile, sourceFile);
     strcat(linuxDestinationFile, ".txt");
     strcat(linuxDestinationFile, "\0");
 
-    printf("%s\n", linuxDestinationFile);
+    printf("file path: %s\n", linuxDestinationFile);
 
     // open linux file
-    FILE* fp = fopen("/home/student/test.txt", "w");
-    if(fp == NULL) printf("we fucked");
+    FILE* fp = fopen(linuxDestinationFile, "w");
+    if(fp == NULL) 
+        printf("***ERROR FILE NOT FOUND***");
 
-    printf("file opened\n");
+    printf("File successfully opened\n");
 
     // create buffers
     char* sourceBuffer = malloc(volume.blockSize);
     char* bodyBuffer = malloc(volume.blockSize);
+    char* lineBuffer = malloc(16);
 
     printf("buffers created\n");
    
+    /* This was not a bad idea, but I dont think we need to get this information
     // get size of file from Filesystem
     long totalSize = getNumberOfBytes("/home/student/HelloWorld.txt");
-    
     int LBAcount = ceil(((double) totalSize) / ((double) volume.blockSize)); 
-    
-    printf("before LBAread\n");
+    printf("Total LBA blocks of file: %d\n", LBAcount);
+    */
 
     // LBAread the total blocks of sourceFile
-    LBAread(sourceBuffer, LBAcount, fileIndex);
-    printf("sourceBuffer: %d\n", sourceBuffer[48]);
-printf("sourceBuffer: %s\n", sourceBuffer);
-    
-    printf("After LBAread before loop\n");
+    LBAread(sourceBuffer, 1, fileIndex);
 
     for(int i=48; i<volume.blockSize; i=i+16) {
-        LBAread(bodyBuffer, 1, 4);
-        printf("bodyBuffer: %s\n", bodyBuffer);
+        if(getLine(sourceBuffer, lineBuffer, i) == 0) continue;
+        LBAread(bodyBuffer, 1, atoi(lineBuffer));
         fputs(bodyBuffer, fp);
-        
-        //if(fgets(bodyBuffer,volume.blockSize,fp) != NULL)
-           // puts(bodyBuffer);
-        printf("after fgets\n");
     }
-    printf("did we make it here\n");
     
-    fclose(fp);
     free(sourceBuffer);
     free(bodyBuffer);
+    free(lineBuffer);
+    fclose(fp);
 
     printf("File successfully copied from Filesystem to LINUX\n");
     return 1;
-    
-    /* Basic steps laid out by Bierman - reference simple shell assignment for guidance
-    srcfd = yourOpen(sourcefile)
-    destfd = open(linuxDestinationFile)
-    yourRead(srcfd, buf, len)
-    write(destfd, buf, len)
-    repeat read/write until entire file copied
-    yourClose(srcfd)
-    close(destfd) */
 }
 
 /*
