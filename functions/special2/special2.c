d1357f1ed77500388e599dac5c93b8d4a8f54f5a
Robin <robert.ian.clarkson@gmail.com>
2020-04-26 20:34:40 -0700

    sudo code

--- functions/special2/special2.c
+++ functions/special2/special2.c
@@ -21,3 +21,23 @@ int special2(struct filesystem_volume volume, struct arguments command) {
     close(destfd) */
     return 1;
 }
+
+/*
+copy file 'foo' in directory 'bar' into '/my/local/path/on/my/computer/')
+Filesystem_Prompt$ cp_to foo root /my/local/path/on/my/computer/
+ /my/local/path/on/my/computer/foo
+ ***Complete***
+
+1. get/check source file index (22 or -1 --> doesnt exist)
+2. check if PATH is valid (I think C has an isValidPath(linuxDestinationFile))
+   - need to check last character of path == '/'--> if != '/' then ERROR, return 0
+3. get fp going with complete path --> fp = open("/my/local/path/on/my/computer/" + "foo", "w+");
+4. LBAread(sourceBuffer, sourceFileIndex)
+5. foreach(body line) --> i = i + 16
+        LBAread(bodyBuffer, line) --> body bytes
+        fp.append(bodyBuffer)
+6. fp.close()
+7. return 1
+    
+
+*/
