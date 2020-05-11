cfe7aec4570a598fc8222ed8ad088ae3c1001551
staaahn <38550876+staaahn@users.noreply.github.com>
2020-05-11 15:40:45 -0700

    Cameron's SFSU ID added

--- functions/cp/copyFile.c
+++ functions/cp/copyFile.c
@@ -6,14 +6,16 @@ Group Name: Orphaned Zombies
 Name: Stanley Brooks
 Student ID: 918764527
 Name: Cameron Harte
-Student ID:
+Student ID: 918245645
 Name: Robert Clarkson
 Student ID:
 Name: Michael Zheng
 Student ID:
 Project: <project name, like Assignment 3 – File System
 File: copyFile.c
-Description: This function takes 4 arguments, source file and folder, and a destination file and folder.  It copies the contents of the source file to the destination file, while keeping the original file intact.
+Description: This function takes 4 arguments, source file and folder, and a destination 
+file and folder.  It copies the contents of the source file to the destination file, while
+keeping the original file intact.
 **************************************************************/
 
 int copyFile(struct filesystem_volume volume, struct arguments command) {
