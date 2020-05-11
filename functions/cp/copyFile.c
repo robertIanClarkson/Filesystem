320ac9d1f83e2fa33d35c1dca9d415b6f7758d1f
staaahn <38550876+staaahn@users.noreply.github.com>
2020-05-11 15:38:32 -0700

    cleaned copy file, added header comment

--- functions/cp/copyFile.c
+++ functions/cp/copyFile.c
@@ -1,20 +1,31 @@
 #include "copyFile.h"
 
-/* (copy file 'foo' as 'blah' from directory 'hello' to directory 'world') */
-/* Filesystem_Prompt$ mkdir hello root */
-/* Filesystem_Prompt$ mkdir world root */
-/* Filesystem_Prompt$ touch foo hello */
-/* Filesystem_Prompt$ cp foo hello bar world */
+/**************************************************************
+Class: CSC-415-03 Spring 2020
+Group Name: Orphaned Zombies
+Name: Stanley Brooks
+Student ID: 918764527
+Name: Cameron Harte
+Student ID:
+Name: Robert Clarkson
+Student ID:
+Name: Michael Zheng
+Student ID:
+Project: <project name, like Assignment 3 – File System
+File: copyFile.c
+Description: This function takes 4 arguments, source file and folder, and a destination file and folder.  It copies the contents of the source file to the destination file, while keeping the original file intact.
+**************************************************************/
+
 int copyFile(struct filesystem_volume volume, struct arguments command) {
     
     /* Checking argc */
     if(command.argc < 5) {
         printf("\t***Not Enough Args***\n");
-        return 0; // check
+        return 0; 
     }
     else if(command.argc > 5){
 	printf("\t***TOO many Args***\n");
-	return 0; //check
+	return 0; 
     }
 
     /* Get args */
@@ -22,43 +33,30 @@ int copyFile(struct filesystem_volume volume, struct arguments command) {
     char* oldFolder = command.args[2];
     char* newFile = command.args[3];
     char* newFolder = command.args[4];
-    // printf("* copy file: %s\n", oldFile);
-    // printf("* from folder: %s\n", oldFolder);
-    // printf("* as file: %s\n", newFile);
-    // printf("* to folder: %s\n", newFolder);
-
-    printf("\tSource File: %s\n", oldFile);
-    printf("\tSource Directory: %s\n", oldFolder);
-    printf("\tNew File: %s\n", newFile);
-    printf("\tTarget Directory: %s\n", newFolder);
 
     /* Get index of 'keyFolder' */
-    // printf("- Looking for old folder\n");
     int oldFolderIndex = getIndex(oldFolder, volume);
     if(oldFolderIndex < 0) {
         printf("\t***FOLDER DNE***\n");
         return 0;
     }
-    // printf("- old folder index: %d\n", oldFolderIndex);
 
     /* get old folder buffer */
     char* oldFolderBuffer = malloc(volume.blockSize);
     int foo = LBAread(oldFolderBuffer, 1, oldFolderIndex);
 
     /* init */
-    // printf("- Getting buffer of \"%s\"\n", oldFile);
     char* childBuffer = malloc(volume.blockSize);
     char* name = malloc(16);
     char* type = malloc(16);
     char* childIndex = malloc(16);
     for(int i = 48; i < volume.blockSize; i = i + 16) { // looking at each child of parent LBA
         if(getLine(oldFolderBuffer, childIndex, i) == 0) continue;
-        /* we have child index */
-        foo = LBAread(childBuffer, 1, atoi(childIndex));
-        /* checkif childBuffer is a folder or file */
+        foo = LBAread(childBuffer, 1, atoi(childIndex)); /* we have child index */
+
         memset(type, 0, 16);
         memset(name, 0, 16);
-        getType(childBuffer, type); // read the type from the childBuffer into "type"
+        getType(childBuffer, type); // read the type from the childBuffer into "type"  ----checkif childBuffer is a folder or file
         if(strcmp(type, "file") == 0) { // child is a file
             getName(childBuffer, name); // read the type from the childBuffer into "type"
             if(strcmp(name, oldFile) == 0) { // THIS IS THE ONE WE WILL COPY 
@@ -68,7 +66,6 @@ int copyFile(struct filesystem_volume volume, struct arguments command) {
     }
     
     /* creating the new file */
-    printf("- Creating new File \"%s\" in \"%s\"\n", newFile, newFolder);
     struct arguments newArgs;
     newArgs.argc = 3;
     strcpy(newArgs.args[0], "touch");
@@ -84,9 +81,6 @@ int copyFile(struct filesystem_volume volume, struct arguments command) {
         return 0;
     }
 
-    /* childBuffer = oldFile */
-    /* newFileBuffer = newFile */
-    printf("- Copying body of \"%s\" to \"%s\"\n", oldFile, newFile);
     char* newFileBuffer = malloc(volume.blockSize);
     int newFileIndex = getIndex(newFile, volume);
     LBAread(newFileBuffer, 1, newFileIndex);
@@ -97,12 +91,10 @@ int copyFile(struct filesystem_volume volume, struct arguments command) {
     char* newFileLine = malloc(16);
     int newBodyIndex;
     for(int i = 48; i < volume.blockSize; i = i + 16) {/* each line oldFile's body */
-        /* get body index from oldFile */
-        if(getLine(childBuffer, oldFileLine, i) == 0) continue;
+        if(getLine(childBuffer, oldFileLine, i) == 0) continue; // get body index from oldFile
 
-        /* read old body index into buffer */
-        foo = LBAread(copyBuffer, 1, atoi(oldFileLine));
 
+        foo = LBAread(copyBuffer, 1, atoi(oldFileLine));  // read old body index into buffer
         /* find an empty LBA */
         newBodyIndex = getNextEmptyLBA(volume);
         if(newBodyIndex < 0) {
@@ -135,6 +127,7 @@ int copyFile(struct filesystem_volume volume, struct arguments command) {
             free(copyBuffer);
             free(oldFileLine);
             free(oldFolderBuffer);
+		
             return 0;
         }
     }    
@@ -147,8 +140,6 @@ int copyFile(struct filesystem_volume volume, struct arguments command) {
     free(copyBuffer);
     free(oldFileLine);
     free(oldFolderBuffer);
-    
-    // printf("***COMPLETE***\n\n");
-
+	
     return 1;
 }
