5dfc5ebc452d3d123f6b6a157d692649d4afa154
staaahn <38550876+staaahn@users.noreply.github.com>
2020-05-11 15:58:54 -0700

    comment block

--- functions/cp/copyFile.c
+++ functions/cp/copyFile.c
@@ -11,7 +11,7 @@ Name: Robert Clarkson
 Student ID:
 Name: Michael Zheng
 Student ID: 917581488
-Project: <project name, like Assignment 3 – File System
+Project: Assignment 3 – File System
 File: copyFile.c
 Description: This function takes 4 arguments, source file and folder, and a destination 
 file and folder.  It copies the contents of the source file to the destination file, while
