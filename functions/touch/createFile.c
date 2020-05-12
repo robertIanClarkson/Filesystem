75527407d0e4ae981c133721220c42dbee607ccf
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 17:02:23 -0700

    Added Block

--- functions/touch/createFile.c
+++ functions/touch/createFile.c
@@ -1,3 +1,20 @@
+/**************************************************************
+Class: CSC-415-03 Spring 2020
+Group Name: Orphaned Zombies
+Name: Stanley Brooks
+Student ID: 918764527
+Name: Cameron Harte
+Student ID: 918245645
+Name: Robert Clarkson
+Student ID: 915433914
+Name: Michael Zheng
+Student ID: 917581488
+Project: Assignment 3 – File System
+File: copyFile.c
+Description: This function takes 4 arguments, source file and folder, and a destination 
+file and folder.  It copies the contents of the source file to the destination file, while
+keeping the original file intact.
+**************************************************************/
 #include "createFile.h"
 
 int createFile(struct filesystem_volume volume, struct arguments command) {
