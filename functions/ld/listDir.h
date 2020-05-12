b3cff1163769db0cde8d1c6f0242fcecc460aadf
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 17:22:05 -0700

    Fixed Added Block

--- functions/ld/listDir.h
+++ functions/ld/listDir.h
@@ -11,9 +11,8 @@ Name: Michael Zheng
 Student ID: 917581488
 Project: Assignment 3 – File System
 File: listDir.h
-Description: This function takes 4 arguments, source file and folder, and a destination 
-file and folder.  It copies the contents of the source file to the destination file, while
-keeping the original file intact.
+Description: This is the header for the listDir function. And these are the function
+declaration
 **************************************************************/
 #ifndef listDir_h
 #define listDir_h
