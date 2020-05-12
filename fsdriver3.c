58df17854a2ecbfd47ee0182ef0cf48d56150d92
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 18:20:37 -0700

    Fixed Added Block

--- fsdriver3.c
+++ fsdriver3.c
@@ -10,10 +10,9 @@ Student ID: 915433914
 Name: Michael Zheng
 Student ID: 917581488
 Project: Assignment 3 – File System
-File: copyFile.c
-Description: This function takes 4 arguments, source file and folder, and a destination 
-file and folder.  It copies the contents of the source file to the destination file, while
-keeping the original file intact.
+File: fsdriver3.c
+Description: This is the driver we used to run the filesystem.
+This includes all of the functions' header file.
 **************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
