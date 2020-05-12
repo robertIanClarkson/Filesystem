f9df3ac85065a692c58efbebcc2944b872c72141
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 17:37:42 -0700

    Fixed Added Block

--- functions/set/setMetaData.h
+++ functions/set/setMetaData.h
@@ -10,10 +10,9 @@ Student ID: 915433914
 Name: Michael Zheng
 Student ID: 917581488
 Project: Assignment 3 – File System
-File: copyFile.c
-Description: This function takes 4 arguments, source file and folder, and a destination 
-file and folder.  It copies the contents of the source file to the destination file, while
-keeping the original file intact.
+File: setMetaData.h
+Description: This is the header for the setMetaData function. And these are the function
+declaration.
 **************************************************************/
 #ifndef setMetaData_h
 #define setMetaData_h
