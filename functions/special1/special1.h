37757e32e76b312a6c68d8a6780d007b567bc310
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 17:41:58 -0700

    Fixed Added Block

--- functions/special1/special1.h
+++ functions/special1/special1.h
@@ -10,10 +10,9 @@ Student ID: 915433914
 Name: Michael Zheng
 Student ID: 917581488
 Project: Assignment 3 – File System
-File: copyFile.c
-Description: This function takes 4 arguments, source file and folder, and a destination 
-file and folder.  It copies the contents of the source file to the destination file, while
-keeping the original file intact.
+File: special1.h
+Description: This is the header for the special1 function. And these are the function
+declaration.
 **************************************************************/
 #ifndef special1_h
 #define special1_h
