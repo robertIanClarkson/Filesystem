6b68ffe0784672e9ff9044a9469e15103d0d5da1
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 17:25:14 -0700

    Fixed Added Block

--- functions/mv/moveFile.c
+++ functions/mv/moveFile.c
@@ -11,9 +11,8 @@ Name: Michael Zheng
 Student ID: 917581488
 Project: Assignment 3 – File System
 File: moveFile.c
-Description: This function takes 4 arguments, source file and folder, and a destination 
-file and folder.  It copies the contents of the source file to the destination file, while
-keeping the original file intact.
+Description: The purpose of the move file function is to relocate a file to a new specified location.  
+It simply copies the file to the new specified location, and then deletes the original source file. 
 **************************************************************/
 #include "moveFile.h"
 
