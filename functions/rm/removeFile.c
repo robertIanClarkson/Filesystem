3e6aa1172f10ef508a9c501e3192779d816b13fa
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 17:33:30 -0700

    Fixed Added Block

--- functions/rm/removeFile.c
+++ functions/rm/removeFile.c
@@ -10,10 +10,9 @@ Student ID: 915433914
 Name: Michael Zheng
 Student ID: 917581488
 Project: Assignment 3 – File System
-File: copyFile.c
-Description: This function takes 4 arguments, source file and folder, and a destination 
-file and folder.  It copies the contents of the source file to the destination file, while
-keeping the original file intact.
+File: removeFile.c
+Description: This function takes 2 arguments the file name and the directory folder.
+This function removes a file.
 **************************************************************/
 #include "removeFile.h"
 
