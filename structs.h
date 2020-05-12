d60b40a2bf7906f49aeb7ae72f01d640365fb53a
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 18:19:27 -0700

    Fixed Added Block

--- structs.h
+++ structs.h
@@ -10,10 +10,9 @@ Student ID: 915433914
 Name: Michael Zheng
 Student ID: 917581488
 Project: Assignment 3 – File System
-File: copyFile.c
-Description: This function takes 4 arguments, source file and folder, and a destination 
-file and folder.  It copies the contents of the source file to the destination file, while
-keeping the original file intact.
+File: structs.h
+Description: This is the header for the structs. And these are the function
+declaration.
 **************************************************************/
 #ifndef stucts_h
 #define stucts_h
