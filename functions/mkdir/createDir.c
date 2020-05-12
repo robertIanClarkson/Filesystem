4773b110c9948d191db7f1c7cfdb6ee9610319a9
Michael Zheng <54775575+MichaelZheng911@users.noreply.github.com>
2020-05-11 17:43:48 -0700

    Update createDir.c

--- functions/mkdir/createDir.c
+++ functions/mkdir/createDir.c
@@ -35,49 +35,36 @@ int createDir(struct filesystem_volume volume, struct arguments command) {
     printf("\tIn Folder: %s\n", parent);
 
     /* Find the first LBA that is empty */
-    // printf("- Looking for Index LBA\n");
     int i;
     for(i = 0; i < volume.blockCount; i++) {
         if(getMap(i, volume) == '0') break;
     }
-    // printf("  - Empty LBA at: %d\n", i);
 
     /* mark LBA as used */
-    // printf("  - Marking LBA as used\n");
     setMap(i, '1', volume);
 
     /* Find another LBA for that is empty for metadata */
-    // printf("- Looking for MetaData LBA\n");
     int j;
     for(j = 0; j < volume.blockCount; j++) {
         if(getMap(j, volume) == '0') break;
     }
-    // printf("  - Empty LBA at: %d\n", j);
 
     /* mark LBA as used */
-    // printf("  - Marking LBA as used\n");
     setMap(j, '1', volume);
 
     /* Get parent LBA position */
-    // printf("- Looking for parent folder\n");
     int parentIndex = getIndex(parent, volume);
     if(parentIndex < 0) {
         printf("\t***PARENT FOLDER DNE***\n");
         return 0;
     }
-    // printf("  - Found parent folder\n");
      
     /* Create Index LBA */
-    // printf("- Creating Buffer\n");
     char* buffer = malloc(volume.blockSize);
     initializeLBA(buffer, '-', volume.blockSize);
-    // printf("  - Adding Name: %s\n", name);
     if(addName(name, buffer) != 1) return 0; // check
-    // printf("  - Adding Type: folder\n");
     if(addType("folder", buffer) != 1) return 0; // check
-    // printf("  - Connecting Metadata LBA index\n");
     if(connectMetaData(j, buffer) != 1) return 0; // check
-    // printf("  - Writing Buffer to LBA: %d\n", i);
     LBAwrite(buffer, 1, i);
     free(buffer);
 
@@ -90,9 +77,7 @@ int createDir(struct filesystem_volume volume, struct arguments command) {
     free(metaBuffer);
 
     /* update parent LBA with child LBA */
-    // printf("- Updating parent folder\n");
     if(addChild(i, parentIndex, volume) != 1) return 0;
 
-    // printf("***COMPLETE***\n\n");
     return 1;
 }
